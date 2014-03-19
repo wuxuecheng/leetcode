5b56c9a362c99dc2e68e8c6aba9fe7c8e2ebd291
wuxuecheng <seemwu@gmail.com>
2014-03-19 17:13:03 +0800

    Create Reverse_Polish_notation.cpp

---
+++ Reverse_Polish_notation.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    void reverseWords(string &s) 
+    {    
+        unsigned int size = s.length();
+        
+        std::string word;
+        
+        std::stack<std::string> wordSet;
+        
+        bool isFirstBlank = false;
+        
+        for (unsigned int i = 0; i<size;++i)
+        {
+            char chr = s[i];
+            
+            if (chr != ' ')
+            {
+                word.push_back(chr);
+                isFirstBlank = true;
+            }
+            else if (chr == ' ' && isFirstBlank)
+            {
+                wordSet.push(word);
+                word = "";
+                isFirstBlank = false;
+            }
+            
+        }
+        if (word != "")
+        wordSet.push(word);
+        
+        s.clear();
+        
+        while(!wordSet.empty())
+        {
+            s.append(wordSet.top());
+            wordSet.pop();
+            if (!wordSet.empty())
+                s.append(" ");
+        }
+    }
+};
