078057f560ed4fb2f4c4422334bd5faee2643baf
wuxuecheng <seemwu@gmail.com>
2015-04-21 17:42:05 +0800

    Create bitwise_and_of_number.cpp

---
+++ bitwise_and_of_number.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        if (m == 0)
+        {
+        	return 0;
+        }
+
+        int orderNumber[8];
+
+        memset(orderNumber, 0, sizeof(orderNumber));
+
+        unsigned int newM = m;
+        unsigned int newN = n;
+
+        int i = 0;
+        for (; i < 8; ++i)
+        {
+        	newN = newN >> (i * 4);
+        	newM = newM >> (i * 4);
+
+        	if (newN - newM < 16)
+        	{
+                int resTmp = newM;
+        		for (unsigned int j = newM + 1; j <= newN; ++j)
+        		{
+        			resTmp = resTmp & j;
+        		}
+        		orderNumber[i] = resTmp;
+
+        		break;
+        	}
+        }
+
+        int res = 0;
+
+        for (int k = i; k >=0; --k)
+        {
+        	res += (orderNumber[k] << (k * 4));
+        }
+
+        return res;
+
+    }
+};
