44967e07213a27659b7400346e339079548c6ab5
wuxuecheng <seemwu@gmail.com>
2015-04-22 14:50:04 +0800

    Create happy_number.cpp

---
+++ happy_number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isHappy(int n);
+private:
+    map<int, int> m_existSums;
+};
+
+bool Solution::isHappy(int n)
+{
+	if (n == 1)
+	{
+		return true;
+	}
+	int sum = 0;
+	while (n >= 10)
+	{
+		int number = n % 10;
+		sum += (number * number);
+		n /= 10;
+	}
+
+	sum += (n * n);
+
+	if (m_existSums.find(sum) != m_existSums.end())
+	{
+		return false;
+	}
+	m_existSums[sum]++;
+
+	return isHappy(sum);
+}
