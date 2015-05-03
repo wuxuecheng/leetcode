02aa2607ca4592d3220a08e50b59e818a2708262
wuxuecheng <seemwu@gmail.com>
2015-05-03 21:34:19 +0800

    add new file: binary_tree_right_side_view.cpp
    Signed-off-by: wuxuecheng <seemwu@gmail.com>

---
+++ binary_tree_right_side_view.cpp
@@ -0,0 +1,61 @@
+/**
+
+ * Definition for a binary tree node.
+
+ * struct TreeNode {
+
+ *     int val;
+
+ *     TreeNode *left;
+
+ *     TreeNode *right;
+
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+
+ * };
+
+ */
+
+class Solution {
+
+public:
+
+    vector<int> rightSideView(TreeNode* root) {
+    m_currentLayer = 0;
+    m_needVals.clear();
+
+    visitTree(root, 0);
+
+    return m_needVals;
+}
+
+    void visitTree(TreeNode* root, int layer)
+    {
+        if (root == NULL)
+        {
+           return;
+        }
+
+        if (layer >= m_currentLayer)
+        {
+            m_needVals.push_back(root->val);
+            ++m_currentLayer;
+        }
+
+        ++layer;
+        if (root->right != NULL)
+        {
+            visitTree(root->right, layer);
+        }
+    
+        
+        if (root->left != NULL)
+        {
+            visitTree(root->left, layer);
+        }
+    }
+
+private:
+    vector<int> m_needVals;
+    int m_currentLayer;
+};
