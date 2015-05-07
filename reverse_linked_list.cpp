44d96df8ea85313fb0281b1f8e9440d6fa723870
wuxuecheng <wuxc@travelsky.com>
2015-05-07 13:00:02 +0800

    add new file reverse_linked_list.cpp
    Signed-off-by: wuxuecheng <wuxc@travelsky.com>

---
+++ reverse_linked_list.cpp
@@ -0,0 +1,43 @@
+/**
+
+ * Definition for singly-linked list.
+
+ * struct ListNode {
+
+ *     int val;
+
+ *     ListNode *next;
+
+ *     ListNode(int x) : val(x), next(NULL) {}
+
+ * };
+
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL)
+        {
+            return NULL;
+        }
+
+        ListNode* currentNode = head;
+        ListNode* nextNode = head->next;
+        ListNode* nextNextNode = NULL;
+
+        while (nextNode != NULL)
+        {
+            nextNextNode = nextNode->next;
+
+            nextNode->next = currentNode;
+
+            currentNode = nextNode;
+
+            nextNode = nextNextNode;
+        }
+
+        head->next = NULL;
+
+        return currentNode;
+    }
+};
