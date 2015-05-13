257d371cbf61584c5bc6c5b39c5cbbd23d811991
wuxuecheng <wuxc@travelsky.com>
2015-05-13 17:11:20 +0800

    concept subarray
    Signed-off-by: wuxuecheng <wuxc@travelsky.com>

---
+++ minimum_size_subarray_sum.cpp
@@ -0,0 +1,34 @@
+class Solution{
+public:
+    int minSubArrayLen(int s, vector<int>& nums)
+    {
+        bool hasFound(false);
+        int sum = 0;
+        int firstPos(0);
+        int minLen = INT_MAX;//#include <climits>
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            sum += nums[i];
+
+            while(sum >= s)
+            {
+                minLen = min(minLen, i - firstPos + 1);
+                sum -= nums[firstPos++];
+
+                if (minLen == 1)
+                {
+                    hasFound = true;
+                    break;
+                }
+            }
+
+            if (hasFound)
+            {
+                break;
+            }
+        }
+
+        return minLen == INT_MAX ? 0 : minLen;
+    }
+};
