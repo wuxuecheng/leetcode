9f8d83379c617ba4cdc85cf38f505dd3ca6e6857
wuxuecheng <seemwu@gmail.com>
2015-06-16 17:34:57 +0800

    add new file invert_binary_tree.cpp
    Signed-off-by: wuxuecheng <seemwu@gmail.com>

---
+++ invert_binary_tree.cpp
@@ -0,0 +1,55 @@
+/**
+
+ * Definition for a binary tree node.
+
+ * struct TreeNode {
+
+ *     int val;
+
+ *     TreeNode *left;
+
+ *     TreeNode *right;
+
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+
+ * };
+
+ */
+
+class Solution {
+
+public:
+
+    TreeNode* invertTree(TreeNode* root) {
+
+    if (root == NULL)
+    {
+        return root;
+    }
+
+    vector<TreeNode*> nodes;
+
+    nodes.push_back(root);
+
+    for (size_t i = 0; i < nodes.size(); ++i)
+    {
+        TreeNode* tmp = nodes[i]->left;
+        nodes[i]->left = nodes[i]->right;
+        nodes[i]->right = tmp;
+        if (nodes[i]->left != NULL)
+        {
+            nodes.push_back(nodes[i]->left);
+        }
+
+        if (nodes[i]->right != NULL)
+        {
+            nodes.push_back(nodes[i]->right);
+        }
+        
+    }
+
+    return root;
+
+    }
+
+};
