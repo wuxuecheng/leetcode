0820d327143b14311c115f46b9c3021f80c7db2b
wuxuecheng <seemwu@gmail.com>
2016-07-15 17:04:13 +0800

    guess number
    Signed-off-by: wuxuecheng <seemwu@gmail.com>

---
+++ guess_number_higher_or_lower.cpp
@@ -0,0 +1,30 @@
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int start = 1;
+        int end = n;
+        int middle = 1;
+         while (1)
+         {
+             middle = (end - start) / 2 + start;
+             if (guess(middle) == 1)
+             {
+                 start = middle + 1;
+             }
+             else if (guess(middle) == -1)
+             {
+                 end = middle - 1;
+             }
+             else
+             {
+                 return middle;
+             }
+          }
+          return middle;
+    }
+};
